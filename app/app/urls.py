af593c181099533f41b9d14b537f31ca2082b0f6
joer9514 <joer9514@gmail.com>
2021-03-12 01:13:08 -0500

    Adding crud

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,21 +17,28 @@ from django.contrib import admin
 from django.urls import path
 from core.user.views import *
 from core.saving.views import *
+from core.category.views import *
 from core.index_page.views import *
 from core.home_page.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', register_user, name='signup'),
+    path('signup/', RegisterCreateUser.as_view(), name='signup'),
     path('login/', login_user, name='login'),
     path('', index_page, name='index'),
-    path('home', home_page, name='home'),
-    path('home/user/add', UserListView.as_view(), name='list_user'),
-    path('home/user/create', UserCreateView.as_view(), name='add_user'),
-    path('home/user/Update', UserUpdateView.as_view(), name='update_user'),
-    path('home/user/delete', UserDeleteView.as_view(), name='delete_user'),
-    path('home/saving/add', SavingListView.as_view(), name='list_saving'),
-    path('home/saving/create', SavingCreateView.as_view(), name='create_saving'),
-    path('home/saving/update', SavingUpdateView.as_view(), name='update_saving'),
-    path('home/saving/delete', SavingDeleteView.as_view(), name='delete_saving'),
+    path('home/', home_page, name='home'),
+    path('home/user/add/', UserListView.as_view(), name='list_user'),
+    path('home/user/create/', UserCreateView.as_view(), name='add_user'),
+    path('home/user/update<int:pk>/', UserUpdateView.as_view(), name='update_user'),
+    path('home/user/delete<int:pk>/', UserDeleteView.as_view(), name='delete_user'),
+
+    path('home/saving/add/', SavingListView.as_view(), name='list_saving'),
+    path('home/saving/create/', SavingCreateView.as_view(), name='create_saving'),
+    path('home/saving/update<int:pk>/', SavingUpdateView.as_view(), name='update_saving'),
+    path('home/saving/delete<int:pk>/', SavingDeleteView.as_view(), name='delete_saving'),
+
+    path('home/category/add/', CategoryListView.as_view(), name='list_category'),
+    path('home/category/create/', CategoryCreateView.as_view(), name='create_category'),
+    path('home/category/update<int:pk>/', CategoryUpdateView.as_view(), name='update_category'),
+    path('home/category/delete<int:pk>/', CategoryDeleteView.as_view(), name='delete_category'),
 ]
--- app/core/user/views.py
+++ app/core/user/views.py
@@ -1,13 +1,55 @@
 # from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from core.user.models import User
 from django.views.generic import *
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
+# from django.utils.decorators import method_decorator
+# from django.contrib.auth.decorators import login_required
+from django.forms import *
 
 
-def register_user(request):
-    return render(request, "user/signup.html")
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+        widgets = {
+            'name_user': TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Name',
+                    'autocomplete': 'off',
+                }
+            ),
+            'surname_user': TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Surname',
+                    'autocomplete': 'off',
+                }
+            ),
+            'email_user': TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Email: test@gmail.com, test@hotmail.com',
+                    'autocomplete': 'off',
+                }
+            ),
+            'password_user': TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Password > 4 characters',
+                    'autocomplete': 'off',
+                }
+            )
+            # Close_dictionary
+        }
+
+
+class RegisterCreateUser(CreateView):
+    model = User
+    form_class = UserForm
+    template_name = 'user/signup.html'
+    success_url = reverse_lazy('login')
 
 
 def login_user(request):
@@ -24,12 +66,21 @@ class UserListView(ListView):
 
 
 class UserCreateView(CreateView):
-    pass
+    model = User
+    form_class = UserForm
+    template_name = 'user/create.html'
+    success_url = reverse_lazy('list_user')
 
 
 class UserUpdateView(UpdateView):
-    pass
+    model = User
+    form_class = UserForm
+    template_name = 'user/update.html'
+    success_url = reverse_lazy('list_user')
 
 
 class UserDeleteView(DeleteView):
-    pass
+    model = User
+    form_class = UserForm
+    template_name = 'user/delete.html'
+    success_url = reverse_lazy('list_user')
