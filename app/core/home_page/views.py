031e8e064cfb960be691b864a0d57586b58da288
Jhonierk <jorge.otalvaro00@usc.edu.co>
2021-03-24 09:30:09 -0500

    add comments in home_pages

--- app/core/home_page/views.py
+++ app/core/home_page/views.py
@@ -3,21 +3,30 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
+#imports of model classes and functions
 from core.saving.views import *
 from core.budget.views import *
 from core.movement.views import *
 from django.views.generic import *
 
-
+#Class that takes the template
 class HomeListView(TemplateView):
     template_name = 'home_page/home.html'
-
+    """
+    function that obtains the objects from the 
+    models and stores them in a dictionary
+    """
     def get_context_data(self, *args, **kwargs):
         budget = Budget.objects.all()
         saving = Saving.objects.all()
         movement = Movement.objects.all()
         return {'budgets': budget, 'savings': saving, 'movements': movement}
 
+    """
+    Decorator method that validates prevents access to the o
+    ther application windows until you log in.
+    """
     @method_decorator(login_required)
+    #Inherits objects from the model
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
